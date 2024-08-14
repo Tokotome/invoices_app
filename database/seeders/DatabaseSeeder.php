79f5a3ce48dfab32252637d47df5e20abe47dad0
Tokotome <mandzhukov.bg@gmail.com>
2024-08-14 13:56:37 +0300

    Add factories, models and seeds

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Counter;
+use App\Models\Invoice;
+use App\Models\Product;
+use App\Models\Customer;
+use App\Models\InvoiceItem;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // \App\Models\Counter::factory(1)->create();
+        // \App\Models\Product::factory(5)->create();
+        // \App\Models\Customer::factory(5)->create();
+        // \App\Models\Invoice::factory(5)->create();
+        \App\Models\InvoiceItem::factory(5)->create();
+    }
+}
